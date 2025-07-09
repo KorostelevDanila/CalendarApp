a946e2baa53b1aa6a69c0cdb2d30f49f33a4d3eb
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-09 13:30:17 +0300

    create IncorrectYearFormatException

---
+++ src/main/java/korostelev_danila/exception/IncorrectYearFormatException.java
@@ -0,0 +1,7 @@
+package korostelev_danila.exception;
+
+public class IncorrectYearFormatException extends RuntimeException {
+    public IncorrectYearFormatException(String message) {
+        super(message);
+    }
+}
