33ed1b643d2c9a6e6ed3dc551dc54474bf98df48
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-09 10:29:37 +0300

    create year-related enums

---
+++ src/main/java/korostelev_danila/model/enums/DayOfWeek.java
@@ -0,0 +1,15 @@
+package korostelev_danila.model.enums;
+
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+    public DayOfWeek next() {
+
+    }
+}
