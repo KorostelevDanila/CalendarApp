83a509e04b576ac8956b9e9080eeca60fc579170
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-09 11:59:07 +0300

    add equals, hashCode and to String to models

--- src/main/java/korostelev_danila/model/Day.java
+++ src/main/java/korostelev_danila/model/Day.java
@@ -2,12 +2,42 @@ package korostelev_danila.model;
 
 import korostelev_danila.model.enums.DayOfWeek;
 
+import java.util.Objects;
+
 public class Day {
-    private String dayNumber;
+    private int dayNumber;
     private DayOfWeek dayOfWeek;
 
-    public Day(String dayNumber, DayOfWeek dayOfWeek) {
+    public Day(int dayNumber, DayOfWeek dayOfWeek) {
         this.dayNumber = dayNumber;
         this.dayOfWeek = dayOfWeek;
+    }
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Day day = (Day) o;
+        return dayNumber == day.dayNumber && dayOfWeek == day.dayOfWeek;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dayNumber, dayOfWeek);
+    }
+
+    @Override
+    public String toString() {
+        return "Day{" +
+                "dayNumber=" + dayNumber +
+                ", dayOfWeek=" + dayOfWeek +
+                '}';
     }
 }
--- src/main/java/korostelev_danila/model/Month.java
+++ src/main/java/korostelev_danila/model/Month.java
@@ -1,6 +1,7 @@
 package korostelev_danila.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Month {
     private String name;
@@ -17,5 +18,25 @@ public class Month {
 
     public List<Day> getDays() {
         return days;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Month month = (Month) o;
+        return Objects.equals(name, month.name) && Objects.equals(days, month.days);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, days);
+    }
+
+    @Override
+    public String toString() {
+        return "Month{" +
+                "name='" + name + '\'' +
+                ", days=" + days +
+                '}';
     }
 }
