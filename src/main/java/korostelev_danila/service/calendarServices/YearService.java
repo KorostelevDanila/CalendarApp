8b42cddf26ca5ce4dfa9c3d2ab3efff406359059
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-09 13:08:21 +0300

    implement year service

--- src/main/java/korostelev_danila/service/calendarServices/YearService.java
+++ src/main/java/korostelev_danila/service/calendarServices/YearService.java
@@ -2,8 +2,17 @@ package korostelev_danila.service.calendarServices;
 
 import korostelev_danila.model.Month;
 import korostelev_danila.model.Year;
+import korostelev_danila.model.enums.Months;
 import korostelev_danila.util.YearConsts;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static korostelev_danila.util.YearConsts.MONTHS_IN_YEAR;
+
+@Service
 public class YearService {
     private final MonthService monthService;
 
@@ -12,6 +21,13 @@ public class YearService {
     }
 
     public Year getYear(String year) {
-        return null;
+        List<Month> monthList = new ArrayList<>();
+        Months currentMonth = Months.JANUARY;
+        for (int i = 0; i < MONTHS_IN_YEAR; i++) {
+            Month month = monthService.getMonth(year, currentMonth);
+            monthList.add(month);
+            currentMonth = Months.values()[currentMonth.ordinal()].next();
+        }
+        return new Year(year, monthList);
     }
 }
