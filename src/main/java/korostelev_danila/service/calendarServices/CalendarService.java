4497b6ef636fe2006ec2f0fa75c5620303f0bf56
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-09 13:30:40 +0300

    implement exception handling for CalendarService

--- src/main/java/korostelev_danila/service/calendarServices/CalendarService.java
+++ src/main/java/korostelev_danila/service/calendarServices/CalendarService.java
@@ -1,5 +1,6 @@
 package korostelev_danila.service.calendarServices;
 
+import korostelev_danila.exception.IncorrectYearFormatException;
 import korostelev_danila.model.Calendar;
 import korostelev_danila.model.Year;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,16 @@ public class CalendarService {
         this.yearService = yearService;
     }
 
-    public Calendar getCalendar(String year) {
+    public Calendar getCalendar(String year) throws IncorrectYearFormatException {
+        try {
+            int integerYear = Integer.parseInt(year);
+            if (integerYear < 1600) {
+                throw new IncorrectYearFormatException("Year is less than 1600, please, enter new value");
+            }
+        } catch (NumberFormatException e) {
+            throw new IncorrectYearFormatException("Incorrect year format, please, enter new value");
+        }
+
         if (lastRequestedYear != null) {
             if (lastRequestedYear.getYear().equals(year)) {
                 return new Calendar(lastRequestedYear);
